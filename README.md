eacaed78ff091275a6acbbe4b3e45d35372f264e
MarcosMDev <marcosmagnopb@gmail.com>
2022-10-20 16:45:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center"> Ignite Timer </h1>
+
+<p align="center">
+Project developed during the specialization in ReactJS module 02 at Rocketseat IGNITEs WEB.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">License</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [ReactJS](https://reactjs.org/)
+- [Styled Components](https://styled-components.com/)
+- [ImmerJS](https://github.com/immerjs/immer)
+- [Node e NPM](https://nodejs.org/)
+- [Vite](https://vitejs.dev/)
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,74 @@
+import * as zod from 'zod'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { HandPalm, Play } from 'phosphor-react'
+import { useContext, useState } from 'react'
+
+import {
+    HomeContainer,
+    StartCountdownButton,
+    StopCountDownButton,
+} from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
+import { CountDown } from './components/CountDown'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+        .number()
+        .min(5, 'O ciclo precisa de no minimo 5 minutos.')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+    const { activeCycle, createNewCycle, interruptCycle } =
+        useContext(CyclesContext)
+
+    const newCycleForm = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        },
+    })
+
+    const { handleSubmit, watch, reset } = newCycleForm
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        createNewCycle(data)
+
+        reset()
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
+    return (
+        <HomeContainer>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+                <FormProvider {...newCycleForm}>
+                    <NewCycleForm />
+                </FormProvider>
+                <CountDown />
+
+                {activeCycle ? (
+                    <StopCountDownButton onClick={interruptCycle} type="button">
+                        <HandPalm size={24} />
+                        Interromper
+                    </StopCountDownButton>
+                ) : (
+                    <StartCountdownButton
+                        disabled={isSubmitDisabled}
+                        type="submit"
+                    >
+                        <Play size={24} />
+                        Começar
+                    </StartCountdownButton>
+                )}
+            </form>
+        </HomeContainer>
+    )
+}
